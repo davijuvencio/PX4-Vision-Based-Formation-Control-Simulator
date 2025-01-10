a1842bf6fc64b457c20ef58914ce1333b522ccc8
davijuvencio <davijuvencio@gmail.com>
2025-01-10 19:56:27 -0300

    PX4 Vision-Based Formation Control Simulator by Davi Juvêncio

---
+++ src/lib/version/build_git_version.h
@@ -0,0 +1,20 @@
+
+/* Auto Magically Generated file */
+/* Do not edit! */
+#pragma once
+
+#define PX4_GIT_VERSION_STR "46a12a09bf11c8cbafc5ad905996645b4fe1a9df"
+#define PX4_GIT_VERSION_BINARY 0x46a12a09bf11c8cb
+#define PX4_GIT_TAG_STR "v1.13.2"
+#define PX4_GIT_BRANCH_NAME "xtdrone/dev"
+
+#define PX4_GIT_OEM_VERSION_STR  ""
+
+#define PX4_GIT_TAG_OR_BRANCH_NAME "v1.13.2" // special variable: git tag, release or master branch
+
+#define MAVLINK_LIB_GIT_VERSION_STR  "3b52eac09c2e37325e4bc49cd2667ea37bf1d7d2"
+#define MAVLINK_LIB_GIT_VERSION_BINARY 0x3b52eac09c2e3732
+
+#define NUTTX_GIT_VERSION_STR  "91bece51afbe7da9db12e3695cdbb4f4bba4bc83"
+#define NUTTX_GIT_VERSION_BINARY 0x91bece51afbe7da9
+#define NUTTX_GIT_TAG_STR  "v11.0.0"
